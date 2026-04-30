06f3efb5b768719c714d59e18f3059eba0b02860
noambrihun <noambrihun1@gmail.com>
2026-04-30 22:11:18 +0300

    fix api route for tasks

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/Ttsks",TaskRoutes)
+app.use("/api/tasks",TaskRoutes)
+
 
 mongoose
 .connect(process.env.MONGO_URI)
